dcb980aeb4c735b1739315e53bc1824cdc2105f3
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2017-03-09 15:34:52 +0700

    Change default animation from material-ui into standalone svg animation

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 // @flow
 
 import React from 'react';
-import CircularProgress from 'material-ui/CircularProgress';
 
 const loadingInjector = (fetchingPropKey: string) => <T>(ComposedComponent: ReactClass<T>): ReactClass<{}> => {
   function WrapperComponent(props: Object) {
     return (
       (props[fetchingPropKey]) ?
-        <div style={{display: 'block', textAlign: 'center'}}><CircularProgress /></div> :
+        (
+          <div style={{display: 'block', textAlign: 'center'}}>
+            <img src="../assets/ring.svg" />
+          </div>
+        ) :
         <ComposedComponent {...props} />
     );
   }
