ab04d924c1193ccb531bf9916e6a9b78032b3b6f
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2017-03-12 03:27:24 +0700

    Add feature for using custom loading indicator

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
 import React from 'react';
 import LoadingIndicator from 'react-loading-indicator';
 
-const loadingInjector = (fetchingPropKey: string) => <T>(ComposedComponent: ReactClass<T>): ReactClass<{}> => {
+const loadingInjector = (fetchingPropKey: string, LoadingComponent?: ReactClass<{}>) => <T>(ComposedComponent: ReactClass<T>): ReactClass<{}> => {
   function WrapperComponent(props: Object) {
+    let child = <LoadingIndicator segments={10} segmentWidth={5} segmentLength={10} />;
+    if (LoadingComponent) {
+      child = <LoadingComponent />;
+    }
     return (
       (props[fetchingPropKey]) ?
         (
           <div style={{display: 'block', textAlign: 'center'}}>
-            <LoadingIndicator segments={10} segmentWidth={5} segmentLength={10} />
+            {child}
           </div>
         ) :
         <ComposedComponent {...props} />
