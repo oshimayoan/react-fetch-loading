34782d8d08be1e389ecd9cc71eebeeda26f3aa17
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2017-03-12 02:55:03 +0700

    Change loading indicator from standalone svg file into react-loading-indicator

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react';
+import LoadingIndicator from 'react-loading-indicator';
 
 const loadingInjector = (fetchingPropKey: string) => <T>(ComposedComponent: ReactClass<T>): ReactClass<{}> => {
   function WrapperComponent(props: Object) {
@@ -8,7 +9,7 @@ const loadingInjector = (fetchingPropKey: string) => <T>(ComposedComponent: Reac
       (props[fetchingPropKey]) ?
         (
           <div style={{display: 'block', textAlign: 'center'}}>
-            <img src="../assets/ring.svg" />
+            <LoadingIndicator segments={10} segmentWidth={5} segmentLength={10} />
           </div>
         ) :
         <ComposedComponent {...props} />
